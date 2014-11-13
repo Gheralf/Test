3f7a6c67bcd0712ea8dab3b8023236a711fc54cf
Gheralf <gheralf@gmail.com>
2014-11-13 21:29:11 +0200

    Toka kommitointi
    Unityn tiedostoja ja vähän sisältöä. Nähdään tuleeko kaikki tavara mitä
    halutaan.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	
+	void Start () {
+		// Nothing here, just me, the test
+	}
+
+	void Update () {
+	
+	}
+}
